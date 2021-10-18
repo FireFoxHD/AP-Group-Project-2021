fa15db35cab37627042c2fac96a223313dcf0bcc
Ricoposse <richrobinson876@gmail.com>
2021-10-17 20:24:24 -0500

    Addition of Equipment Class
    Added the Equipment class with working CRUD operations, minor changes to the database to fix spelling errors in some field names of the "equipment_table" table

---
+++ EquipmentRental/src/testing/TestingDriver.java
@@ -0,0 +1,30 @@
+package testing;
+
+import java.sql.Connection;
+
+import classes.Equipment;
+
+public class TestingDriver {
+static Connection connection = null;
+	
+	public static void main(String[] args) {
+		
+		//Driver and package just to test method functionality until GUI is implemented
+		//May be deleted when no longer necessary
+		
+		
+		/*System.out.println("im runnung");
+		Equipment equip = new Equipment();
+		equip.create("1002", "Sound", "20 ft Speaker Box", 24000, "Not Booked");
+		equip.create("1000", "Lighting", "30 ft Light Array", 9000, "Booked");
+		equip.readAll();
+		equip.update("1000", "Lighting", "30 ft Light Array", 9000, "Not Booked");
+		equip.readAll();
+		equip.create("1003", "Lighting", "30 ft Sting Lights", 7000, "Not Booked");
+		equip.readAll();
+		equip.readOne("1000");
+		equip.update("1003", "Lighting", "20 ft String Lights", 8000, "Not Booked");
+		equip.readAll();
+		equip.Delete("1003");*/
+	}
+}
